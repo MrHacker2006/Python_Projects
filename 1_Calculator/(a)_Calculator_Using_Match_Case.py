7fb0825cd829fd15ec37c0242c42802bd53ccc23
Gautam Gupta <guptagautam908@gmail.com>
2025-07-17 10:20:19 +0530

    Basic Calculator Generated

--- 1_Calculator/(a)_Calculator_Using_Match_Case.py
+++ 1_Calculator/(a)_Calculator_Using_Match_Case.py
@@ -1,3 +1,16 @@
-print(5)
-print("Hello World")
-print("Gautam")
+# Making Calculator using Match Case.
+
+a = int(input("Enter The Value of a: "))
+b = int(input("Enter The Value of b: "))
+
+operation = int(input("Enter 1 for Addition\nEnter 2 for Subtraction\nEnter 3 for Multiplication\nEnter 4 for Division\n"))
+
+match operation:
+    case 1:
+        print(f"The Addtion of a and b is {a+b}")
+    case 2:
+        print(f"The Subtraction of a and b is {a-b}")
+    case 3:
+        print(f"The Multiplication of a and b is {a*b}")
+    case 4:
+        print(f"The Division of a and b is {a/b}")
