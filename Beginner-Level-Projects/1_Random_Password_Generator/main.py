e594111f8cfce11bbeaf4b3d81cb8d928c4974f3
Gautam Gupta <guptagautam908@gmail.com>
2025-08-06 07:53:07 +0530

    We have Sucessfully stored the inputs taken from the user in form of booleans

--- Beginner-Level-Projects/1_Random_Password_Generator/main.py
+++ Beginner-Level-Projects/1_Random_Password_Generator/main.py
@@ -4,7 +4,7 @@ import string
 import sys
 import pyperclip
 
-# Taking Input from the user
+# Taking Input from the user and storing it in the form of boolean
 print("Welcome to the Random Password Generator!\n")
 while True:
     try:
@@ -12,19 +12,14 @@ while True:
         if (length>0):
                 print("\nWhat things you want in your password? Answer below👇\n")
 
-                choice_Number = str(input("Do you want that your password contains number? Enter Y/N: "))
-                choice_UpperCase = str(input("Do you want that your password contains UpperCase character? Enter Y/N: "))
-                choice_LowerCase = str(input("Do you want that your password contains LowerCase character? Enter Y/N: "))
-                choice_Symbol = str(input("Do you want that your password contains Symbols? Enter Y/N: "))
-                choice_Special_Symbol = str(input("Do you want that your password contains Special Symbols Enter Y/N: "))
+                include_number = input("Do you want to include Numbers ? (Y/N): ").strip().lower() in ['y', 'yes']
+                include_lowercase = input("Do you want to include Lowercase Letters ? (Y/N): ").strip().lower() in ['y', 'yes']
+                include_uppercase = input("Do you want to include Uppercase letters ? (Y/N): ").strip().lower() in ['y', 'yes']
+                include_normal_symbol = input("Do you want to include Normal Symbols ? (Y/N): ").strip().lower() in ['y', 'yes']
+                include_special_symbol = input("Do you want to include Special Symbols ? (Y/N): ").strip().lower() in ['y', 'yes']
                 break
         else:
              print("Password length must be greater than zero")
     except ValueError:
         print("Please enter a vaild Natural Number!")
     
-# Storing the choices in form of boolean variables
-a=choice_Number.lowercase()
-print(a)
-print(b)
-print(c)
